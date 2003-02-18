2e1315eb9a66079b157b9eba2c2f94cea3435916
alexw <alexw>
2003-02-18 15:01:29 +0000

    adds

---
+++ dvb/include/linux/dvb/avia/avia_gt_fb.h
@@ -0,0 +1,9 @@
+#ifndef __FB_H__
+#define __FB_H__
+
+#define AVIA_GT_GV_SET_BLEV	0	/* blend level */
+#define AVIA_GT_GV_SET_POS	1	/* position of graphics frame */
+#define AVIA_GT_GV_HIDE		2	/* hide framebuffer */
+#define AVIA_GT_GV_SHOW		3	/* show framebuffer */
+
+#endif /* __FB_H__ */
