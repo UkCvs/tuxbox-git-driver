e5b886eb784bc2710d2f9d03afe585799d2c1dd6
zwen <zwen>
2003-03-19 19:43:12 +0000

    - forgot some lines while applying last delta

--- avia/avia_gt_fb_core.c
+++ avia/avia_gt_fb_core.c
@@ -21,6 +21,9 @@
  *
  *
  *   $Log: avia_gt_fb_core.c,v $
+ *   Revision 1.38.2.4  2003/03/19 19:43:12  zwen
+ *   - forgot some lines while applying last delta
+ *
  *   Revision 1.38.2.3  2003/03/05 09:12:12  zwen
  *   - eNX red & blue swap fix
  *   - fixed mmio address for eNX (by obi)
@@ -176,7 +179,7 @@
  *   Revision 1.7  2001/01/31 17:17:46  tmbinc
  *   Cleaned up avia drivers. - tmb
  *
- *   $Revision: 1.38.2.3 $
+ *   $Revision: 1.38.2.4 $
  *
  */
 
@@ -415,6 +418,7 @@ static int avia_gt_fb_encode_var(struct fb_var_screeninfo *var, const void *fb_p
 
 	var->red.length = 5;
 	var->red.msb_right = 0;
+   var->red.offset = 10;
 
 	var->green.length = 5;
 	var->green.msb_right = 0;
@@ -422,17 +426,7 @@ static int avia_gt_fb_encode_var(struct fb_var_screeninfo *var, const void *fb_p
 
 	var->blue.length = 5;
 	var->blue.msb_right = 0;
-
-	if (avia_gt_chip(GTX))
-	{
-		var->red.offset = 10;
-		var->blue.offset = 0;
-	}
-	else /* if (avia_gt_chip(ENX)) */
-	{
-		var->red.offset = 0;
-		var->blue.offset = 10;
-	}
+   var->blue.offset = 0;
 
 	var->transp.length = 1;
 	var->transp.msb_right = 0;
@@ -718,7 +712,7 @@ static struct fb_ops avia_gt_fb_ops = {
 int __init avia_gt_fb_init(void)
 {
 
-	printk("avia_gt_fb: $Id: avia_gt_fb_core.c,v 1.38.2.3 2003/03/05 09:12:12 zwen Exp $\n");
+	printk("avia_gt_fb: $Id: avia_gt_fb_core.c,v 1.38.2.4 2003/03/19 19:43:12 zwen Exp $\n");
 
 	gt_info = avia_gt_get_info();
 
